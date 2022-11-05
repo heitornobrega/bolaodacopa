ff0b78d4289c69df8a583fa1c52e4b09f943462e
heitornobrega <nobheitor@gmail.com>
2022-11-05 18:56:49 -0300

    server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,26 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient({
+    log: ['query']
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     })
 
-    fastify.get('/pools/count', () => {
-        return {count: 15645456465}
+    await fastify.register(cors, {
+        origin: true,
     })
 
-    await fastify.listen({port: 3333})
+    fastify.get('/pools/', async () => {
+        const pools = await prisma.pool.count()
+        return pools;
+    })
+
+    await fastify.listen({port: 3333, host: '0.0.0.0'})
 }
 
 bootstrap();
