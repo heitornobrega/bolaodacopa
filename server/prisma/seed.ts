5415602e194d6c5d642c5ad268ab59102dc98c7b
heitornobrega <nobheitor@gmail.com>
2022-11-06 21:13:56 -0300

    home finalizada

---
+++ server/prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Fulano da Silva",
+      email: "fulano@gmail.com",
+      avatarUrl: "",
+    },
+  });
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Bolão 1",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participant: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  const game = await prisma.game.create({
+    data: {
+      date: "2022-11-02T12:00:00.201Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-03T12:00:00.201Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          particpant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
